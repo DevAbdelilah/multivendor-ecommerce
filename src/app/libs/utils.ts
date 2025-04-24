03a2e7371a3b3d81aedd2a804c60b18abadd77fb
DevAbdelilah <charafiabdelilah3@gmail.com>
2025-04-24 16:55:00 +0100

    setup project

---
+++ src/app/libs/utils.ts
@@ -0,0 +1,8 @@
+// Merges Tailwind classes (used by ShadCN components)
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
